04904a1a5000a94c151c263624e2191c5cf54d8a
Alll Rafsann <141483613+al-hasby@users.noreply.github.com>
2025-05-21 13:34:40 +0700

    Add files via upload
    punya al

---
+++ ecommers-2/toko/toko.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+  </head>
+  <body>
+<div class="container flex flex-wrap justify-around mt-15 ml-15 gap-6">
+
+
+<div class="w-xs border rounded-sm shadow-lg overflow-hidden flex-wrap ">
+<div class="w-full h-[180px]  ">
+    <img src="images.jpeg" alt="" >
+</div>
+<h3 class="text-2xl py-5"><b>awooooo</b></h3>
+<p class="text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam excepturi, reiciendis molestiae ea nobis quas quis repellat mollitia nemo atque modi amet quidem? Numquam, repellat! Deleniti consectetur eaque eum ullam.</p>
+</div>
+
+<div class="w-xs border rounded-sm shadow-lg overflow-hidden flex-wrap">
+<div class="w-full h-[180px]  ">
+    <img src="images.jpeg" alt="" >
+</div>
+<h3 class="text-2xl py-5"><b>awooooo</b></h3>
+<p class="text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam excepturi, reiciendis molestiae ea nobis quas quis repellat mollitia nemo atque modi amet quidem? Numquam, repellat! Deleniti consectetur eaque eum ullam.</p>
+</div>
+
+
+</div>
+  </body>
+</html>
